10caa1a6aa912bd2b2c83769c12dd8b6f0a617df
darkwhole33 <abhishekadhav04@gmail.com>
2024-12-11 12:40:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo  "Cloning Repo...."
-git clone https://github.com/darkwhole33/abhishek01/ /MissPerfectURLBOT
-cd /MissPerfectURLBOT
+git clone https://github.com/darkwhole33/abhishek01/ /abhishek01
+cd /abhishek01
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
